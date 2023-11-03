131171ddaa61d59835a7dc2d22c7e205fd0d8098
Max Schneider <maxschneidercodes@gmail.com>
2023-11-03 10:39:32 +0100

    fix

--- src/prismicio.js
+++ src/prismicio.js
@@ -43,9 +43,8 @@ export const createClient = (config) => {
   const client = prismic.createClient(repositoryName, {
     routes,
     fetchOptions:
-      process.env.NODE_ENV === "production"
-        ? { next: { tags: ["prismic"] }, cache: "force-cache" }
-        : { next: { revalidate: 5 } },
+      //? { next: { tags: ["prismic"] }, cache: "force-cache" }
+      { next: { revalidate: 5 } },
     ...config,
   });
 
